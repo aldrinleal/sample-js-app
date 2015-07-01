34e7279d6c82281fc4b09eb9941b98fee5a98fe4
Aldrin Leal <aldrin@leal.eng.br>
2015-07-01 04:55:19 -0300

    First Version

---
+++ app.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.send(JSON.stringify({
+  	version: process.version
+  }));
+});
+
+app.get('/memory', function (req, res) {
+  res.send(JSON.stringify({
+  	version: process.memoryUsage()
+  }));
+});
+
+var server = app.listen(port, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
